4b5543421cf184617af32939dd5d2d7cde329efb
Doug Becker <112132281+DougB-MN@users.noreply.github.com>
2022-08-25 19:04:44 -0500

    Update streamlit_app.py
    fix emojii

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ st.title('My Parents New Healthy Diner')
 
 st.header('Breakfast Menu')
 
-st.text('	:bowl_with_spoon: Omega 3 and Blueberry Cereal')
-st.text(':green_salad: Kale, Spinach & Rocket Smoothie')
-st.text(':egg: Hard-Boiled Free-Range Egg')
+st.text('\N{bowl_with_spoon} Omega 3 and Blueberry Cereal')
+st.text('\N{green_salad} Kale, Spinach & Rocket Smoothie')
+st.text('\N{egg} Hard-Boiled Free-Range Egg')
