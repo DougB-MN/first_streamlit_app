b268891f9ed818803a2698a58c704ff217c7ebf5
Doug Becker <112132281+DougB-MN@users.noreply.github.com>
2022-08-25 19:06:24 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,5 @@ st.title('My Parents New Healthy Diner')
 st.header('Breakfast Menu')
 
 st.text('\N{flexed biceps} Omega 3 and Blueberry Cereal')
-
+st.text('\N{green salad} Kale, Spinach & Rocket Smoothie')
+st.text('\N{egg} Hard-Boiled Free-Range Egg')
