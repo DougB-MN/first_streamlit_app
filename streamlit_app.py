2d0c9c4b0daba85674f9b4dd38653d70e67cf784
Doug Becker <112132281+DougB-MN@users.noreply.github.com>
2022-08-30 18:57:24 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,5 +12,7 @@ st.text('\N{egg} Hard-Boiled Free-Range Egg')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_frulit_list = my_fruit_list.set_index('Fruit')
 
+show my_fruit_list.index
+
 st.multiselect("Pick some fruits:", list(my_fruit_list.index))
 st.dataframe(my_fruit_list)
