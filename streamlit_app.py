b05e0cbb68769830691b86e92b3de0ea9404fa7e
Doug Becker <112132281+DougB-MN@users.noreply.github.com>
2022-09-01 21:19:56 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,4 +17,4 @@ fruits_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index),
 
 fruits_to_show = my_fruit_list.loc(1)
 
-st.dataframe(fruits_to_show)
+#st.dataframe(fruits_to_show)
