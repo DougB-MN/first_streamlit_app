9944491ed2b1f7df6e1c11fe0e57a4e86f59f95d
Doug Becker <112132281+DougB-MN@users.noreply.github.com>
2022-09-01 21:57:27 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,6 +22,8 @@ st.dataframe(df)
 fruit_choice = st.text_input('What fruit would you like information about?', 'Kiwi')
 st.write('The user entered ', fruit_choice)
 
+fruityvice_response = r.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 
 st.dataframe(fruityvice_normalized)
